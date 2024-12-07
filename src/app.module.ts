88992979bdb01115d41dd027e301b79b2d58a7b2
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-12-07 22:11:56 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,26 +23,36 @@ import { Gift } from './point/entity/gift.entity';
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: configService.get<'postgres'>('DATABASE_TYPE'),
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        entities: [
-          User,
-          Otp,
-          FoodHistory,
-          Food,
-          FoodGroup,
-          FoodRate,
-          ScanHistory,
-          PointHistory,
-          Gift,
-        ],
-        synchronize: true,
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const isUsingUnixSocket = configService.get<boolean>('USE_UNIX_SOCKET'); // Flag untuk memilih Unix socket
+        const socketPath = `/cloudsql/${configService.get<string>('INSTANCE_CONNECTION_NAME')}`; // Path ke Unix socket
+
+        return {
+          type: configService.get<'postgres'>('DATABASE_TYPE'),
+          host: isUsingUnixSocket
+            ? undefined
+            : configService.get<string>('DATABASE_HOST'),
+          port: isUsingUnixSocket
+            ? undefined
+            : configService.get<number>('DATABASE_PORT'),
+          extra: isUsingUnixSocket ? { socketPath } : undefined,
+          username: configService.get<string>('DATABASE_USERNAME'),
+          password: configService.get<string>('DATABASE_PASSWORD'),
+          database: configService.get<string>('DATABASE_NAME'),
+          entities: [
+            User,
+            Otp,
+            FoodHistory,
+            Food,
+            FoodGroup,
+            FoodRate,
+            ScanHistory,
+            PointHistory,
+            Gift,
+          ],
+          synchronize: true,
+        };
+      },
       inject: [ConfigService],
     }),
     MailerModule.forRootAsync({
