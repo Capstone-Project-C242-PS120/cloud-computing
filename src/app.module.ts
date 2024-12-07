6280e17ccdadb2c184cbee4c16f39a9ecb178a98
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-12-08 00:49:59 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,65 @@
 import { Module } from '@nestjs/common';
-
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entity/user.entity';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
-import * as path from 'path';
 import { Otp } from './auth/entity/otp.entity';
-import { AuthModule } from './auth/auth.module';
-
-import { FoodModule } from './food/food.module';
 import { FoodHistory } from './food/entity/food-history.entity';
 import { Food } from './food/entity/food.entity';
 import { FoodGroup } from './food/entity/food-group.entity';
 import { FoodRate } from './food/entity/food-rate.entity';
 import { ScanHistory } from './food/entity/scan-history.entity';
-import { PointModule } from './point/point.module';
-
 import { PointHistory } from './point/entity/point-history.entity';
 import { Gift } from './point/entity/gift.entity';
+import { AuthModule } from './auth/auth.module';
+import { FoodModule } from './food/food.module';
+import { PointModule } from './point/point.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import * as path from 'path';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
         const isUsingUnixSocket = configService.get<boolean>('USE_UNIX_SOCKET'); // Flag untuk memilih Unix socket
-        const socketPath = `/cloudsql/${configService.get<string>('INSTANCE_CONNECTION_NAME')}`; // Path ke Unix socket
+        const socketPath = `/cloudsql/${configService.get<string>('INSTANCE_CONNECTION_NAME')}`;
+
+        console.log('isUsingUnixSocket:', isUsingUnixSocket);
+        console.log('Socket Path:', socketPath);
+
+        // Pastikan semua variabel yang diperlukan ada
+        const databaseType = configService.get<'postgres'>('DATABASE_TYPE');
+        const host = configService.get<string>('DATABASE_HOST');
+        const port = configService.get<number>('DATABASE_PORT');
+        const username = configService.get<string>('DATABASE_USERNAME');
+        const password = configService.get<string>('DATABASE_PASSWORD');
+        const databaseName = configService.get<string>('DATABASE_NAME');
+
+        console.log('Database Type:', databaseType);
+        console.log(
+          'Database Host:',
+          isUsingUnixSocket === true ? 'Unix Socket' : host,
+        );
+        console.log(
+          'Database Port:',
+          isUsingUnixSocket === true ? 'Unix Socket' : port,
+        );
+        console.log('Database Username:', username);
+        console.log('Database Name:', databaseName);
 
         return {
-          type: configService.get<'postgres'>('DATABASE_TYPE'),
-          host: isUsingUnixSocket
-            ? undefined
-            : configService.get<string>('DATABASE_HOST'),
-          port: isUsingUnixSocket
-            ? undefined
-            : configService.get<number>('DATABASE_PORT'),
-          extra: isUsingUnixSocket ? { socketPath } : undefined,
-          username: configService.get<string>('DATABASE_USERNAME'),
-          password: configService.get<string>('DATABASE_PASSWORD'),
-          database: configService.get<string>('DATABASE_NAME'),
+          type: databaseType,
+          host: isUsingUnixSocket === true ? undefined : host, // Gunakan `undefined` jika menggunakan Unix socket
+          port: isUsingUnixSocket === true ? undefined : port, // Gunakan `undefined` jika menggunakan Unix socket
+          extra: isUsingUnixSocket === true ? { socketPath } : undefined, // Gunakan socketPath jika menggunakan Unix socket
+          username: username,
+          password: password,
+          database: databaseName,
           entities: [
             User,
             Otp,
@@ -50,11 +71,12 @@ import { Gift } from './point/entity/gift.entity';
             PointHistory,
             Gift,
           ],
-          synchronize: true,
+          synchronize: true, // Sesuaikan dengan kebutuhan Anda
         };
       },
       inject: [ConfigService],
     }),
+
     MailerModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -73,7 +95,7 @@ import { Gift } from './point/entity/gift.entity';
           from: configService.get<string>('DEFAULT_FROM'),
         },
         template: {
-          dir: path.join(__dirname, '../src/auth/templates'), // Sesuaikan dengan lokasi template kamu
+          dir: path.join(__dirname, '../src/auth/templates'),
           adapter: new PugAdapter(),
           options: {
             strict: true,
@@ -82,10 +104,8 @@ import { Gift } from './point/entity/gift.entity';
       }),
       inject: [ConfigService],
     }),
+
     AuthModule,
-    ConfigModule.forRoot({
-      isGlobal: true, // Membuat konfigurasi tersedia di seluruh modul
-    }),
     FoodModule,
     PointModule,
   ],
