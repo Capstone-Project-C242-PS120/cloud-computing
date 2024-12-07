45628316120d4d93680cd027bdf87842cecba902
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-12-08 01:59:48 +0800

    tes

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,8 +16,8 @@ import { PointModule } from './point/point.module';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 import * as path from 'path';
-import { Connector, IpAddressTypes } from '@google-cloud/cloud-sql-connector';
-import { Pool } from 'pg';
+// import { Connector, IpAddressTypes } from '@google-cloud/cloud-sql-connector';
+// import { Pool } from 'pg';
 
 @Module({
   imports: [
@@ -28,44 +28,15 @@ import { Pool } from 'pg';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
-        const isUsingUnixSocket = configService.get<string>('USE_UNIX_SOCKET'); // Flag untuk memilih Unix socket
+        const isCloudRrun = configService.get<string>('CLOUD_RUN'); // Flag untuk memilih Unix socket
         const socketPath = `/cloudsql/${configService.get<string>('INSTANCE_CONNECTION_NAME')}`;
-
-        console.log('isUsingUnixSocket:', isUsingUnixSocket);
-        console.log('Socket Path:', socketPath);
-
-        // Menggunakan cloud-sql-connector untuk menghubungkan ke PostgreSQL
-
-        const connector = new Connector();
-        const clientOpts = await connector.getOptions({
-          instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
-          ipType: IpAddressTypes.PSC,
-        });
-        const pool = new Pool({
-          ...clientOpts,
-          user: configService.get<string>('DATABASE_USERNAME'),
-          database: configService.get<string>('DATABASE_NAME'),
-        });
-
-        // Menguji koneksi pertama kali
-        await pool.connect().catch((err) => {
-          console.error('Database connection error:', err);
-          throw err;
-        });
-
-        console.log('Connected to the database successfully');
-
         return {
           type: 'postgres',
           host:
-            isUsingUnixSocket === 'true'
-              ? undefined
+            isCloudRrun === 'true'
+              ? socketPath
               : configService.get<string>('DATABASE_HOST'),
-          port:
-            isUsingUnixSocket === 'true'
-              ? undefined
-              : configService.get<number>('DATABASE_PORT'),
-          extra: isUsingUnixSocket === 'true' ? { socketPath } : undefined,
+          port: configService.get<number>('DATABASE_PORT'),
           username: configService.get<string>('DATABASE_USERNAME'),
           password: configService.get<string>('DATABASE_PASSWORD'),
           database: configService.get<string>('DATABASE_NAME'),
